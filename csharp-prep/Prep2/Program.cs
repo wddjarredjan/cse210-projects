83a459e178fdc83470815efae50c1f38e91f723c
wddjarredjan <jarredjohnmadelabernal@gmail.com>
2023-05-05 13:28:34 +0800

    Program.cs

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,22 @@
 using System;
 
-class Program
+public class Resume
 {
-    static void Main(string[] args)
+    public string _name;
+
+    // Make sure to initialize your list to a new List before you use it.
+    public List<Job> _jobs = new List<Job>();
+
+    public void Display()
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine($"Name: {_name}");
+        Console.WriteLine("Jobs:");
+
+        // Notice the use of the custom data type "Job" in this loop
+        foreach (Job job in _jobs)
+        {
+            // This calls the Display method on each job
+            job.Display();
+        }
     }
 }
